e3c2ffd0be0fe21437a42702daac5b9b32f466d1
unknown <sequeirajosecarlos@gmail.com>
2021-02-08 10:35:51 -0600

    primer commit

---
+++ PrimerProyectoGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimerProyectoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
